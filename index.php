756e92f53a4d56650ca873f88073de2070d8cee2
Aucho98 <augustolorenzov@gmail.com>
2023-06-27 14:57:33 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heroes</title>
+</head>
+<body>
+    <h2>superman</h2>
+    <h2>literna verde</h2>
+    <h2>flash</h2>
+    <h2>batman</h2>
+    <h2>aquaman</h2>
+    <h2>heman</h2>
+    <h2>ironman</h2>
+</body>
+</html>
